6e72d046cc41d436e08d5e239e5bb93c67ed7d40
Francesco <95973256+Torluch@users.noreply.github.com>
2024-10-22 16:24:31 +0200

    Update 1.cpp

--- Atcoder/375/1.cpp
+++ Atcoder/375/1.cpp
@@ -1,1 +1,18 @@
-a
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main(){
+    int n; cin >> n; 
+    
+    vector<char> v(n); 
+    for(char &i: v) cin >> i; 
+
+    int ans = 0; 
+    for(int i = 1; i < n - 1; i++){
+        if(v[i] == '#') continue; 
+
+        if(v[i - 1] == v[i + 1] && v[i - 1] == '#') ans++; 
+    }
+
+    cout << ans << endl; 
+}
