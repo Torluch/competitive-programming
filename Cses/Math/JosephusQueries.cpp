9bc6030071f98a9a9253255bff34cf6271772a3a
Francesco <95973256+Torluch@users.noreply.github.com>
2024-11-20 18:15:45 +0100

    Create JosephusQueries.cpp

---
+++ Cses/Math/JosephusQueries.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+void solve(){
+    int n, k; cin >> n >> k; 
+ 
+    function <int (int, int)> f = [&](int n, int rim) -> int {
+        if(n == 1) return 1; 
+        if(rim <= (n + 1) / 2) return ((2*rim > n) ? (2*rim % n) : 2*rim); 
+        
+        return( 2 * f(n / 2, rim - (n + 1)/2 ) + ((n % 2) ? 1 : -1)); 
+    }; 
+ 
+    cout << f(n, k) << endl; 
+}
+ 
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+  int t; cin >> t; 
+  while(t--) solve();
