19dd2cf7807e595f20f99bd0254f52581cb21141
Francesco <95973256+Torluch@users.noreply.github.com>
2024-11-24 16:46:00 +0100

    Create Tower_of_Hanoi.cpp

---
+++ Cses/Introductory Problems/Tower_of_Hanoi.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main(){
+    int n; cin >> n; 
+    
+    vector<pair<int, int>> v; 
+
+    function<void (int, int, int, int)> f = [&](int n, int start, int end, int intermedium)  {
+        if(n == 1) {
+            v.push_back({start + 1, end + 1});
+            return;  
+        }
+
+        f(n - 1, start, intermedium, end); 
+        v.push_back({start + 1, end + 1});
+        f(n - 1, intermedium, end, start);  
+    }; 
+    f(n, 0, 2, 1); 
+
+    cout << v.size() << endl; 
+    for(auto i: v) cout << i.first << " " << i.second << endl; 
+}
